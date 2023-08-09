2c072f842f619232fcef6a9bac5dab419c964ffc
DESKTOP-F0SIRI9\Thai Bao <nguyenthaibao157@gmail.com>
2023-08-09 21:32:56 +0700

    Update code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=bao123456789
+export POSTGRES_HOST=image-gallery-database.cteera5wuhj3.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=image_gallery_database
+export AWS_BUCKET=project3-968227778347
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=SpjphqEu0hzgblcoN7DQfMxugMLVhzrBp+rfZs09
+export URL=http://localhost:8100
