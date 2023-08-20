582e852319a283a6c457405369bcb1f7d305a209
DESKTOP-F0SIRI9\Thai Bao <nguyenthaibao157@gmail.com>
2023-08-21 00:37:25 +0700

    Update BE URL Cloud

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa1094407864d41c486aca3453b29cc1-1600130729.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
